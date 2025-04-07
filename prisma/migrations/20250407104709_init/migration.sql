54f40be7f13d29859b2fcd71a857b2f3326c2562
Marcio Abreu <marcioabreu.ba@gmail.com>
2025-04-07 11:07:32 -0300

    Adicionar schema.prisma e arquivos de migração

--- prisma/migrations/20250407104709_init/migration.sql
+++ prisma/migrations/20250407104709_init/migration.sql
@@ -1,14 +1,28 @@
 -- CreateTable
 CREATE TABLE "Event" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" SERIAL NOT NULL,
     "eventName" TEXT NOT NULL,
-    "eventId" TEXT,
-    "eventTime" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "userData" JSONB,
-    "customData" JSONB,
-    "serverData" JSONB,
-    "pixelSent" BOOLEAN NOT NULL DEFAULT false,
-    "cAPIsSent" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "userData" JSONB NOT NULL DEFAULT '{}',
+    "customData" JSONB NOT NULL DEFAULT '{}',
+    "serverData" JSONB NOT NULL DEFAULT '{}',
+    "status" TEXT NOT NULL DEFAULT 'pending',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "PixelCode" (
+    "id" SERIAL NOT NULL,
+    "eventName" TEXT NOT NULL,
+    "domain" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PixelCode_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PixelCode_eventName_domain_key" ON "PixelCode"("eventName", "domain");
